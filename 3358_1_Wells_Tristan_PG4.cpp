d281fba234dbf930ffa6890c9232e2f955aae6af
Twells2153 <43145319+Twells2153@users.noreply.github.com>
2019-10-20 18:37:57 -0500

    Update 3358_1_Wells_Tristan_PG4.cpp

--- 3358_1_Wells_Tristan_PG4.cpp
+++ 3358_1_Wells_Tristan_PG4.cpp
@@ -1,10 +1,133 @@
-// A CPP program to demonstrate linked list
-// based implementation of queue
 #include <iostream>
-#include "Queue.h"
-#include "Stack.h"
 using namespace std;
 
+class SNode{
+    public:
+        int data;
+        SNode* next;
+};
+
+struct SNode* top;
+class Stack{
+public:
+    SNode *front;
+    SNode *rear;
+
+    SNode* newNode(int k)
+    {
+
+        SNode* temp = new SNode();
+            temp->data = k;
+            temp->next = NULL;
+            return temp;
+    }
+
+    Stack* createStack()
+    {
+        Stack* s = new Stack();
+        s->front = s->rear = NULL;
+        return s;
+    }
+
+    void push(Stack* s, int k)
+    {
+        // Create a new LL node
+        SNode* temp = newNode(k);
+
+        // If queue is empty, then
+        // new node is front and rear both
+        if (s->rear == NULL) {
+            s->front = s->rear = temp;
+            return;
+        }
+        s->rear->next = temp;
+        s->rear = temp;
+    }
+
+    SNode* pop(Stack* s)
+    {
+        // If queue is empty, return NULL.
+        if (s->front == NULL)
+            return NULL;
+
+        SNode* temp = s->rear;
+        delete(temp);
+
+        s->rear = s->rear->next;
+
+        // If front becomes NULL, then
+        // change rear also as NULL
+        if (s->rear == NULL)
+            s->front = NULL;
+        return temp;
+    }
+};
+
+
+class QNode {
+public:
+	int data;
+	QNode* next;
+};
+
+class Queue {
+public:
+	QNode *front;
+	QNode *rear;
+
+    QNode* newNode(int k)
+    {
+        QNode* temp = new QNode();
+        temp->data = k;
+        temp->next = NULL;
+        return temp;
+    }
+
+    Queue* createQueue()
+    {
+        Queue* q = new Queue();
+        q->front = q->rear = NULL;
+        return q;
+    }
+
+    void enQueue(Queue* q, int k)
+    {
+        // Create a new LL node
+        QNode* temp = newNode(k);
+
+        // If queue is empty, then
+        // new node is front and rear both
+        if (q->rear == NULL) {
+            q->front = q->rear = temp;
+            return;
+        }
+
+        // Add the new node at
+        // the end of queue and change rear
+        q->rear->next = temp;
+        q->rear = temp;
+    }
+
+    QNode* deQueue(Queue* q)
+    {
+        // If queue is empty, return NULL.
+        if (q->front == NULL)
+            return NULL;
+
+        // Store previous front and
+        // move front one node ahead
+        QNode* temp = q->front;
+        delete(temp);
+
+        q->front = q->front->next;
+
+        // If front becomes NULL, then
+        // change rear also as NULL
+        if (q->front == NULL)
+            q->rear = NULL;
+        return temp;
+    }
+};
 
 // Driver code
 int main()
@@ -71,4 +194,3 @@ int main()
 		cout << "Dequeued item is " << n->data;
 	return 0;
 }
-
